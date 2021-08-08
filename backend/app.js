aaf11109b1e096b84f500406fceede90c83d08cf
Andriatsimaniry <niryomega@free.fr>
2021-08-08 22:59:02 +0200

    Cookie session

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,23 @@
 //Appel des modules
-const express = require("express"); //Importer express 
+const express = require("express"); //Importer express
 const mongoose = require("mongoose");
 const sauceRoutes = require("./routes/sauces");
 const userRoutes = require("./routes/user");
 const helmet = require("helmet");
-const session = require('cookie-session');
+const session = require("cookie-session");
 const path = require("path");
 const rateLimit = require("express-rate-limit");
 
 // utilisation du module 'dotenv' pour masquer les informations de connexion à la base de données à l'aide de variables d'environnement
-require('dotenv').config();
-
+require("dotenv").config();
 
 // Connection à la base de donnée
 mongoose
-  .connect(process.env.MD_UTIL,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    }
-  )
+  .connect(process.env.MD_UTIL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
 
@@ -45,14 +42,24 @@ app.use((req, res, next) => {
 // X-Download-Option,Cache-Control,Content-Type-Options,X-frame-Option,X-Xss-Protection
 app.use(helmet());
 
-
-
-
+// Protection pour eviter les vol de sessions
+const expiryDate = new Date(Date.now() + 60 * 60 * 1000); // 1 heure
+app.use(
+  session({
+    name: "session",
+    secret: process.env.CK_SES,
+    cookie: {
+      secure: true,
+      httpOnly: true,
+      domain: "http://localhost:3000",
+      expires: expiryDate,
+    },
+  })
+);
 
 // utiliser reponse json
-app.use(express.json());  //Transformer le corps de la requette en objet javascript utilisable
+app.use(express.json()); //Transformer le corps de la requette en objet javascript utilisable
 app.use(express.urlencoded({ extended: true }));
-
 
 //Appel de l'API
 
@@ -64,9 +71,5 @@ app.use("/images", express.static(path.join(__dirname, "images")));
 
 // Répond toutes les sauces
 app.use("/api/sauces", sauceRoutes);
-
-
-
-
 
 module.exports = app; //Pour pouvoir acceder depuis des autres fichiers notament serveur Node
